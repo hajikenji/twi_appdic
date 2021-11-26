b97b75d15a412ad12234567575800d2a91a05a80
kenji hajiro <mademoiselle3824@gmail.com>
2021-11-26 18:07:47 +0900

    instans become object

--- app/controllers/twi_controlls_controller.rb
+++ app/controllers/twi_controlls_controller.rb
@@ -1,12 +1,16 @@
 class TwiControllsController < ApplicationController
+  before_action :set_model_twi, only: [:new]
   def index
     @twiindex = Twimodel.all
   end
   def new
-    @twinew = Twimodel.new
   end
   def create
     Twimodel.create(content: params[:twimodel][:content])
     redirect_to "/twi_controlls"
+  end
+  private
+  def set_model_twi
+    @view_model_twis = Twimodel.new
   end
 end
