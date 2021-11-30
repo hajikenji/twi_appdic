810a36bd67e7a1ab5d7b434e9d406e0f14c93975
kenji hajiro <mademoiselle3824@gmail.com>
2021-11-30 23:59:30 +0900

    confirm完成

--- app/controllers/twi_controlls_controller.rb
+++ app/controllers/twi_controlls_controller.rb
@@ -1,22 +1,34 @@
 class TwiControllsController < ApplicationController
   before_action :set_model_twi, only: [:new]
+
   def index
     @twiindex = TwiControll.all
   end
+  
   def new
   end
+
   def create
-    TwiControll.create(content: params[:twi_controll][:content])
-    redirect_to "/twi_controlls"
+    if params[:back]
+      @view_model_twis = TwiControll.new(content: params[:twi_controll][:content])
+      render :new
+    elsif
+      TwiControll.create(content: params[:twi_controll][:content])
+      redirect_to "/twi_controlls"
+    else
+      binding.pry
+    end
   end
+
   def confirm
-    @view_model_twis = TwiControll.new
-    @kkk = TwiControll.new(params[:twi_controll][:content])
+    @view_model_twis = TwiControll.new(content: params[:twi_controll][:content])
     # binding.pry
 
   end
+
   private
   def set_model_twi
     @view_model_twis = TwiControll.new
   end
+
 end
